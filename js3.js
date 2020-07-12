f55eeb4251778907fc97c25cf671eb4d06843184
sftvov-bruno <123@mail.ru>
2020-07-12 13:51:17 +0300

    finish lesson 3

--- js3.js
+++ js3.js
@@ -1,13 +1,33 @@
 function randomNumber() {
-    return Math.round(Math.random() * 256);
+    return Math.round(Math.random() * 255);
 }
 
 function changeColor() {
     this.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
 }
 
-let todoWraper = document.querySelector('.todo-wraper');
+let todoWraper = document.querySelector('.change-color-div');
 todoWraper.addEventListener('click', changeColor);
 
-//Случайная смена цвета по клику дива
+//Сделали случайную смену цвета по клику дива
+//---------------------------------------------
+function previousSlide() {
+    debugger
+    let arrOfClass = this.nextElementSibling.children[0].classList;
+    arrOfClass.forEach(y)
+    console.log(arrOfClass);
+}
+
+let arrowLeft = document.querySelector('.left');
+arrowLeft.addEventListener('click', previousSlide)
+
+//Сделать слайдер с плавной анимацией!!!!!!!!!!!!!!!!!!!!!!!!!!
+//---------------------------------------------
+
+
+//Сделать todo list!!!!!!!!!!!!!!!!!!!!!!!!!!
+//---------------------------------------------
+
+
+//Сделать черную тему через селектор!!!!!!!!!!!!!!!!!!!!!!!!!!
 //---------------------------------------------
