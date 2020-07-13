cceb4e2453d17f688272a8dcd12e0d10eb56a4c5
sftvov <63662363+sftvov@users.noreply.github.com>
2020-07-13 22:53:00 +0300

    slider is finished

--- js3.js
+++ js3.js
@@ -11,15 +11,74 @@ todoWraper.addEventListener('click', changeColor);
 
 //Сделали случайную смену цвета по клику дива
 //---------------------------------------------
-function previousSlide() {
-    debugger
+// function previousSlide() {
+//     let arrOfSlide = document.querySelectorAll('.slide');
+//     for (let i=0; i<arrOfSlide.length; i++) {
+//         if (arrOfSlide[i].classList.contains('active')) {
+//             arrOfSlide[i].classList.remove('active');
+//             if (i - 1 == -1) {
+//                 i = arrOfSlide.length - 1;
+//             } else {
+//                 i--;
+//             }
+//             arrOfSlide[i].classList.add('active');
+//             break;
+//         }
+//     }
+// }
+// function nextSlide() {
+//     let arrOfSlide = document.querySelectorAll('.slide');
+//     for (let i=0; i<arrOfSlide.length; i++) {
+//         if (arrOfSlide[i].classList.contains('active')) {
+//             arrOfSlide[i].classList.remove('active');
+//             if (i + 1 == arrOfSlide.length) {
+//                 i = 0;
+//             } else {
+//                 i++;
+//             }
+//             arrOfSlide[i].classList.add('active');
+//             break;
+//         }
+//     }
+// }
+
+// let arrowLeft = document.querySelector('.left');
+// arrowLeft.addEventListener('click', previousSlide);
+// let arrowRight = document.querySelector('.right');
+// arrowRight.addEventListener('click', nextSlide);
+
+//сделана первая версия слайдера
+//----------------------------------------
+
+function moveSlide() {
     let arrOfSlide = document.querySelectorAll('.slide');
-    // arrOfSlide.forEach(x => checkSlide(x));
-    console.log(arrOfSlide);
+    for (let i=0; i<arrOfSlide.length; i++) {
+        if (arrOfSlide[i].classList.contains('active')) {
+            arrOfSlide[i].classList.remove('active');
+            if (this.classList.contains('left')) {
+                if (i - 1 == -1) {
+                    i = arrOfSlide.length - 1;
+                } else {
+                    i--;
+                }                
+            } else {
+                if (i + 1 == arrOfSlide.length) {
+                    i = 0;
+                } else {
+                    i++;
+                }
+            }
+            arrOfSlide[i].classList.add('active');
+            break;
+        }
+    }
 }
 
-let arrowLeft = document.querySelector('.left');
-arrowLeft.addEventListener('click', previousSlide)
+let arrowBtns = document.querySelectorAll('.arrow');
+for (let i = 0; i < arrowBtns.length; i++) {
+    const el = arrowBtns[i];
+    el.addEventListener('click', moveSlide);
+}
 
 //Сделать слайдер с плавной анимацией!!!!!!!!!!!!!!!!!!!!!!!!!!
 //---------------------------------------------
