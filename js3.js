105a22db27598893d0e474abdfe137c7ee4b35e5
sftvov <63662363+sftvov@users.noreply.github.com>
2020-07-15 21:55:07 +0300

    first succses

--- js3.js
+++ js3.js
@@ -49,41 +49,76 @@ todoWraper.addEventListener('click', changeColor);
 
 //сделана первая версия слайдера с двумя функциями
 //----------------------------------------
-function delClasses(i) {
-    arrOfSlide[i].classList.remove('active');
-    arrOfSlide[i].classList.remove('right-position');
-}
+
+// function moveSlide() {
+//     for (let i=0; i<arrOfSlide.length; i++) {
+//         if (arrOfSlide[i].classList.contains('active')) {
+//             arrOfSlide[i].classList.remove('active');                      
+//             if (this.classList.contains('left')) { //проверка на какую стрелку нажали влево или вправо
+//                 if (i - 1 == -1) { //Проверка на крайний слайд
+//                     i = arrOfSlide.length - 1;
+//                 } else {
+//                     i--;
+//                 }                
+//             } else {
+//                 if (i + 1 == arrOfSlide.length) {//Проверка на крайний слайд
+//                     i = 0;
+//                 } else {
+//                     i++;
+//                 }
+//             }
+//             arrOfSlide[i].classList.add('active');
+//             break;
+//         }
+//     }
+// }
+// const arrOfSlide = document.querySelectorAll('.slide');
+// let arrowBtns = document.querySelectorAll('.arrow');
+// for (let i = 0; i < arrowBtns.length; i++) {
+//     const el = arrowBtns[i];
+//     el.addEventListener('click', moveSlide);
+// }
+
+//Сделал слайдер с одной функцией для двух кнопок
+//---------------------------------------------
 function moveSlide() {
     for (let i=0; i<arrOfSlide.length; i++) {
         if (arrOfSlide[i].classList.contains('active')) {
-            arrOfSlide[i].classList.add('right-position');
-            setTimeout(delClasses, 2000, i);            
+            arrOfSlide[i].classList.add('smooth');
             if (this.classList.contains('left')) { //проверка на какую стрелку нажали влево или вправо
+                debugger
+                arrOfSlide[i].classList.add('right-position');
                 if (i - 1 == -1) { //Проверка на крайний слайд
-                    i = arrOfSlide.length - 1;
+                    newActiveSlide = arrOfSlide.length - 1;
                 } else {
-                    i--;
-                }                
+                    newActiveSlide--;
+                }
+                arrOfSlide[newActiveSlide].classList.add('active');
+                arrOfSlide[newActiveSlide].classList.add('left-position');
+                arrOfSlide[newActiveSlide].classList.add('smooth');
+                setTimeout(() => {
+                    arrOfSlide[newActiveSlide].classList.remove('left-position');
+                }, 0);     
             } else {
                 if (i + 1 == arrOfSlide.length) {//Проверка на крайний слайд
-                    i = 0;
+                    newActiveSlide = 0;
                 } else {
-                    i++;
+                    newActiveSlide++;
                 }
             }
-            arrOfSlide[i].classList.add('active');
-            break;
+                        break;
         }
     }
 }
 const arrOfSlide = document.querySelectorAll('.slide');
-let arrowBtns = document.querySelectorAll('.arrow');
+let newActiveSlide = 0;
+const arrowBtns = document.querySelectorAll('.arrow');
 for (let i = 0; i < arrowBtns.length; i++) {
     const el = arrowBtns[i];
     el.addEventListener('click', moveSlide);
 }
 
-//Сделать слайдер с одной функцией для двух кнопок и плавной анимацией!!!!!!!!!!!!!!!!!!!!!!!!!!
+//Сделать слайдер с плавной прокруткой
 //---------------------------------------------
 
 
