a7a0cfa34783cd246d7ceee785a6cd46ee72a538
sftvov <63662363+sftvov@users.noreply.github.com>
2020-07-17 21:28:54 +0300

    finish my homework

--- js3.js
+++ js3.js
@@ -11,144 +11,37 @@ todoWraper.addEventListener('click', changeColor);
 
 //Сделали случайную смену цвета по клику дива
 //---------------------------------------------
-// function previousSlide() {
-//     let arrOfSlide = document.querySelectorAll('.slide');
-//     for (let i=0; i<arrOfSlide.length; i++) {
-//         if (arrOfSlide[i].classList.contains('active')) {
-//             arrOfSlide[i].classList.remove('active');
-//             if (i - 1 == -1) {
-//                 i = arrOfSlide.length - 1;
-//             } else {
-//                 i--;
-//             }
-//             arrOfSlide[i].classList.add('active');
-//             break;
-//         }
-//     }
-// }
-// function nextSlide() {
-//     let arrOfSlide = document.querySelectorAll('.slide');
-//     for (let i=0; i<arrOfSlide.length; i++) {
-//         if (arrOfSlide[i].classList.contains('active')) {
-//             arrOfSlide[i].classList.remove('active');
-//             if (i + 1 == arrOfSlide.length) {
-//                 i = 0;
-//             } else {
-//                 i++;
-//             }
-//             arrOfSlide[i].classList.add('active');
-//             break;
-//         }
-//     }
-// }
 
-// let arrowLeft = document.querySelector('.left');
-// arrowLeft.addEventListener('click', previousSlide);
-// let arrowRight = document.querySelector('.right');
-// arrowRight.addEventListener('click', nextSlide);
-
-//сделана первая версия слайдера с двумя функциями
-//----------------------------------------
-
-// function pressArrowBtn() {
-//     for (let i=0; i<arrOfSlide.length; i++) {
-//         if (arrOfSlide[i].classList.contains('active')) {
-//             arrOfSlide[i].classList.remove('active');                      
-//             if (this.classList.contains('left')) { //проверка на какую стрелку нажали влево или вправо
-//                 if (i - 1 == -1) { //Проверка на крайний слайд
-//                     i = arrOfSlide.length - 1;
-//                 } else {
-//                     i--;
-//                 }                
-//             } else {
-//                 if (i + 1 == arrOfSlide.length) {//Проверка на крайний слайд
-//                     i = 0;
-//                 } else {
-//                     i++;
-//                 }
-//             }
-//             arrOfSlide[i].classList.add('active');
-//             break;
-//         }
-//     }
-// }
-// const arrOfSlide = document.querySelectorAll('.slide');
-// let arrowBtns = document.querySelectorAll('.arrow');
-// for (let i = 0; i < arrowBtns.length; i++) {
-//     const el = arrowBtns[i];
-//     el.addEventListener('click', pressArrowBtn);
-// }
-
-//Сделал слайдер с одной функцией для двух кнопок
-//---------------------------------------------
-// function removeClasses() {
-//     for (let i=0; i<arrOfSlide.length; i++) {
-//         if (arrOfSlide[i].classList.contains('left-position') || arrOfSlide[i].classList.contains('right-position')) {
-//             arrOfSlide[i].classList.remove('active', 'left-position', 'right-position');  
-//         }
-//     }
-// }
-// function pressArrowBtn() {
-//     removeClasses();
-//     debugger
-//     for (let i=0; i<arrOfSlide.length; i++) {        
-//         if (arrOfSlide[i].classList.contains('active')) {
-//             if (this.classList.contains('left')) { //проверка на какую стрелку нажали влево или вправо
-//                 arrOfSlide[i].classList.add('right-position');
-//                 if (i - 1 == -1) { //Проверка на крайний слайд
-//                     newActiveSlide = arrOfSlide.length - 1;
-//                 } else {
-//                     newActiveSlide--;
-//                 }
-//                 arrOfSlide[newActiveSlide].classList.add('left-position', 'active');
-//                 setTimeout(() => {
-//                     arrOfSlide[newActiveSlide].classList.remove('left-position');
-//                 }, 0);     
-//             } else {
-//                 arrOfSlide[i].classList.add('left-position');
-//                 if (i + 1 == arrOfSlide.length) {//Проверка на крайний слайд
-//                     newActiveSlide = 0;
-//                 } else {
-//                     newActiveSlide++;
-//                 }
-//                 arrOfSlide[newActiveSlide].classList.add('right-position', 'active');
-//                 setTimeout(() => {
-//                     arrOfSlide[newActiveSlide].classList.remove('right-position');
-//                 }, 0);  
-//             }
-//             break;
-//         }
-//     }
-// }
-// const arrOfSlide = document.querySelectorAll('.slide');
-// let newActiveSlide = 0;
-// const arrowBtns = document.querySelectorAll('.arrow');
-// for (let i = 0; i < arrowBtns.length; i++) {
-//     const el = arrowBtns[i];
-//     el.addEventListener('click', pressArrowBtn);
-// }
-//Сделан слайдер с плавной прокруткой
-//---------------------------------------------
-
+function removeClasses() {
+    for (let i=0; i<arrOfSlide.length; i++) {
+        if (arrOfSlide[i].classList.contains('left-position') || arrOfSlide[i].classList.contains('right-position')) {
+            arrOfSlide[i].classList.remove('active', 'left-position', 'right-position');  
+        }
+    }
+}
 function pressArrowBtn() {
+    removeClasses();
     for (let i=0; i<arrOfSlide.length; i++) {        
         if (arrOfSlide[i].classList.contains('active')) {
             if (this.classList.contains('left')) { //проверка на какую стрелку нажали влево или вправо
                 arrOfSlide[i].classList.add('right-position');
                 if (i - 1 == -1) { //Проверка на крайний слайд
                     newActiveSlide = arrOfSlide.length - 1;
-                } else {
+                } 
+                else {
                     newActiveSlide--;
                 }
                 arrOfSlide[newActiveSlide].classList.add('left-position', 'active');
                 setTimeout(() => {
                     arrOfSlide[newActiveSlide].classList.remove('left-position');
                 }, 0);     
-            } else {
+            } 
+            else {
                 arrOfSlide[i].classList.add('left-position');
                 if (i + 1 == arrOfSlide.length) {//Проверка на крайний слайд
                     newActiveSlide = 0;
-                } else {
+                } 
+                else {
                     newActiveSlide++;
                 }
                 arrOfSlide[newActiveSlide].classList.add('right-position', 'active');
@@ -167,12 +60,66 @@ for (let i = 0; i < arrowBtns.length; i++) {
     const el = arrowBtns[i];
     el.addEventListener('click', pressArrowBtn);
 }
-//Оптимизировать код плавного слайдера!!!!!!!!!!
+
+//Сделан слайдер с плавной прокруткой
 //---------------------------------------------
 
-//Сделать todo list!!!!!!!!!!!!!!!!!!!!!!!!!!
+function setTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(receive));
+}
+function createNewItem(el_receive) {
+    let todo_wraper = document.createElement('div');
+    todo_wraper.className = 'todo-wraper';
+    todo_wraper.id = `wraper-${i}`;
+    document.querySelector('.todo-box').appendChild(todo_wraper);
+    let todo_item = document.createElement('div');
+    todo_item.className = 'todo-item';
+    todo_item.innerText = el_receive;
+    todo_item.id = `item-${i}`;
+    document.querySelector(`#wraper-${i}`).appendChild(todo_item);
+    let todo_btn_remove = document.createElement('button');
+    todo_btn_remove.className = 'btn-remove';
+    todo_btn_remove.innerText = 'X';
+    todo_btn_remove.id = `btn-${i}`;
+    document.querySelector(`#wraper-${i}`).appendChild(todo_btn_remove);
+    i++;  
+    } 
+function addNewTask() {
+    let todo_text = document.querySelector('.todo-input').value; 
+    receive.push(todo_text);
+    setTasksToLocalStorage()
+    createNewItem(todo_text);
+    document.querySelector('.todo-input').value = '';
+    }
+function remover(event) {
+    let number_of_id = event.target.id.slice(4);
+    $(`#wraper-${number_of_id}`).remove();
+    delete receive[number_of_id];
+    setTasksToLocalStorage()
+}
+let i = 0;
+let receive = JSON.parse(localStorage.getItem('tasks'));
+if (receive === null) receive = []; 
+else {
+    receive = receive.filter(x => x!== null);
+    setTasksToLocalStorage()
+    receive.forEach(createNewItem);
+    }
+$('body').on('click', '.btn-remove', remover);
+
+//Сделал todo list
 //---------------------------------------------
 
+function changeTheme() {
+    if (this.selectedOptions[0].value == 1){
+        document.querySelector('.todo').classList.add('dark');
+    }
+    else {
+        document.querySelector('.todo').classList.remove('dark');
+    }
+}
+const themeSelector = document.querySelector('select');
+themeSelector.addEventListener('change', changeTheme);
 
-//Сделать черную тему через селектор!!!!!!!!!!!!!!!!!!!!!!!!!!
+//Сделал темную тему через селектор
 //---------------------------------------------
